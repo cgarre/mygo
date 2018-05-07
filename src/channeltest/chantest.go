8e977ee1b5296d574b7ca18ee467a861c5b5b537
Chandra Garre <cgarre@gmail.com>
2018-05-07 14:23:37 -0700

    testing channels

---
+++ src/channeltest/chantest.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func doSomething(s chan string) {
+	time.Sleep(time.Second)
+	fmt.Print(" --- 1 --- ")
+	s <- "done"
+}
+
+func doSomethingElse(s chan string) {
+	time.Sleep(time.Second + time.Second)
+	fmt.Print(" --- 2 --- ")
+	s <- "done"
+
+}
+
+//simple test
+func main() {
+	chan1 := make(chan string, 2)
+	chan2 := make(chan string)
+
+	go doSomething(chan1)
+	go doSomethingElse(chan2)
+
+	<-chan1
+	fmt.Print("ok...")
+	<-chan2
+	chan1 <- "d"
+	<-chan1
+
+	fmt.Print("Done ... ")
+}
